eb3bc50b28f148d95b6952466cc3d9f700f47650
matthias <matthias.dfrc.ch>
2020-03-16 23:08:28 +0900

    2020.03.16 1. faver composition is over inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,7 @@
 import { MatchReader } from "./MatchReader";
-import {MatchResult } from './MatchResult';
+import { Summary } from "./Summary";
 
-const reader = new MatchReader("football.csv");
-reader.read();
-
-console.log(reader.data);
-
-let manUnitedWins = 0;
-
-for (let match of reader.data) {
-  if (match[1] === "Man united" && match[5] === "H") {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === "A") {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+const matchReader = MatchReader.fromCsv("football.csv");
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
